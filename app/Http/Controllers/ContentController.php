b367a8a74f5a5454e078f5776ee66eada7dfbd85
Bernardo Azevedo <bernardoazevedo.8@gmail.com>
2025-03-23 10:38:41 -0300

    movendo css para arquivo separado e aplicando mudanças, usando ParsedownExtra no lugar de Parsedown

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -2,40 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Content;
-use Exception;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redirect;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\View\View;
-use Parsedown;
+use ParsedownExtra;
 
 class ContentController extends Controller
 {
-    /**
-     * 
-     */
     public function index(){
         return view('index');
     }
 
-    /**
-     * Parse a Markdown text to HTML
-     * @param  String $markdownText the Markdown text to be parsed to HTML
-     * @return String 
-     */
     public function markdownToHtmlAjax(Request $request): String {
         return $this->markdownToHtml($request->markdownText);
     }
 
-    /**
-     * Parse a Markdown text to HTML
-     * @param  String $markdownText the Markdown text to be parsed to HTML
-     * @return String 
-     */
     private function markdownToHtml(String $markdownText): String {
-        $parsedown = new Parsedown();
+        $parsedown = new ParsedownExtra();
         $htmlText  = $parsedown->text($markdownText);
         return $htmlText;
     }
