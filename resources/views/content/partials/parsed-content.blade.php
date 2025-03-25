ff99fa8bb138f5048167d0862bde280661acee3c
Bernardo Azevedo <bernardoazevedo.8@gmail.com>
2025-03-25 20:46:51 -0300

    traduzindo textos em português para inglês

--- resources/views/content/partials/parsed-content.blade.php
+++ resources/views/content/partials/parsed-content.blade.php
@@ -1,5 +1,5 @@
 <section id="preview-container" class="p-4 sm:p-8 shadow sm:rounded-lg col-span-2">
     <div id="htmlText" class="parsedown flex flex-col gap-y-2 sm:w-auto">
-        O conteúdo formatado aparecerá aqui
+        Formatted content will appear here
     </div>
 </section>
--- resources/views/content/partials/write-content.blade.php
+++ resources/views/content/partials/write-content.blade.php
@@ -2,7 +2,7 @@
     <div class="space-y-6 grow-wrap dont-break-out">
         <textarea id="text" name="text" type="text" rows="7" cols=""
             class="p-4 sm:p-8 bg-white sm:rounded-lg border-none focus:border-gray-400 focus:ring-gray-400 shadow block w-full"
-            required autofocus autocomplete="text" placeholder="Digite o conteúdo aqui"
+            required autofocus autocomplete="text" placeholder="Type here"
             onInput="this.parentNode.dataset.replicatedValue = this.value"></textarea>
     </div>
 </section>
