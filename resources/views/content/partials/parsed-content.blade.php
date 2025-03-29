29067680b1c473b4ca23f6525484c041091b6af6
Bernardo Azevedo <bernardoazevedo.8@gmail.com>
2025-03-29 19:12:53 -0300

    voltando ao layout antigo

--- resources/views/content/partials/parsed-content.blade.php
+++ resources/views/content/partials/parsed-content.blade.php
@@ -1,4 +1,4 @@
-<section id="preview-container" class="p-4 sm:p-8 shadow sm:rounded-lg col-span-2 h-dvh overflow-y-auto">
+<section id="preview-container" class="p-4 sm:p-8 shadow sm:rounded-lg col-span-2">
     <div id="htmlText" class="parsedown flex flex-col gap-y-2 sm:w-auto">
         Formatted content will appear here
     </div>
--- resources/views/editor.blade.php
+++ resources/views/editor.blade.php
@@ -1,14 +1,14 @@
 <x-app-layout>
-    <div class=" mx-auto sm:px-6 lg:px-8 space-y-6">
-        <form action="/generatePdf" method="post">
-            @csrf
-            <div class="lg:grid lg:grid-cols-5 lg:space-x-4 space-y-6 lg:space-y-0">
-                @include('content.partials.style-editor')
-
-                @include('content.partials.write-content')
-
-                @include('content.partials.parsed-content')
-            </div>
-        </form>
+    <div class="py-8">
+        <div class=" mx-auto sm:px-6 lg:px-8 space-y-6">
+            <form action="/generatePdf" method="post">
+                @csrf
+                <div class="lg:grid lg:grid-cols-5 lg:space-x-4 space-y-6 lg:space-y-0">
+                    @include('content.partials.style-editor')
+                    @include('content.partials.write-content')
+                    @include('content.partials.parsed-content')
+                </div>
+            </form>
+        </div>
     </div>
 </x-app-layout>
