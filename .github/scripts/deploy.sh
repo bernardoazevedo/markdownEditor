a92e3fb67fb29e7cb48e0ee8d197d6266ea13310
Bernardo Azevedo <111990904+bernardoazevedo@users.noreply.github.com>
2025-04-04 20:25:13 -0300

    Update deploy.sh

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -9,7 +9,7 @@ wget -qO- https://cdn.rawgit.com/creationix/nvm/master/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-nvm install 16
+nvm install 18
 
 source ~/.bashrc
 
