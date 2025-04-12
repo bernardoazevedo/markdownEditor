22ccbc1da6bd97dce821ba45a08462d22620ff99
Bernardo Azevedo <bernardoazevedo.8@gmail.com>
2025-04-12 12:05:15 -0300

    ajuste de padding e texto do botão download

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-stone-100 mb-16">
+        <div class="min-h-screen bg-stone-100 pb-28">
 
             @include('layouts.navigation')
 
