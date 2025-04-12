62b268ad7afe5e36ed1d64317fcbf277e241842d
Bernardo Azevedo <bernardoazevedo.8@gmail.com>
2025-04-12 11:21:51 -0300

    alterando display da barra superior

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -3,7 +3,8 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between items-center h-16 text-stone-700">
             <img src="{{ asset('assets/images/logo.svg') }}" alt="mdf2pdf logo" width="80px">
-            <h1 class="text-lg font-semibold">Edit text in markdown and convert to PDF</h1>
+
+            <h1 class="text-lg font-semibold hidden sm:block">Edit text in markdown and convert to PDF</h1>
 
             <a href="https://github.com/bernardoazevedo/markdownEditor" target="_blank">
                 <img src="{{ asset('assets/images/github.svg') }}" alt="Github Logo" width="24px" height="24px">
